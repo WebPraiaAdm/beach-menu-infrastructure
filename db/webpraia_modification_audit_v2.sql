e15b8fb13a81bb0a14a048e9d43db4252df1f2ad
artaraujo_meli <runageinc@gmail.com>
2025-01-16 21:08:10 -0300

    Adding infrastructure changes

---
+++ db/webpraia_modification_audit_v2.sql
@@ -0,0 +1,17 @@
+create table if not exists "customer_session_aud" (
+    "id" bigint generated always as identity primary key,
+    "role" varchar(150) not null default 'USER',
+    "customer_id" bigint not null,
+    "name" varchar(255) not null,
+    "refresh_token" text not null,
+    "expiration" timestamptz not null,
+    "revoked" boolean not null default false,
+    "created_at" timestamptz default now(),
+    "updated_at" timestamptz default now(),
+    "deleted_at" timestamptz default null,
+    "created_by" varchar(255) not null,
+    "updated_by" varchar(255) not null,
+    "deleted_by" varchar(255) default null,
+    "rev" int not null,
+    "revtype" smallint null
+);
--- db/webpraia_modification_v2.sql
+++ db/webpraia_modification_v2.sql
@@ -4,6 +4,22 @@ create table if not exists "otp_temporary_code" (
 	"created_at" timestamptz default now ()
 );
 
+create table if not exists "customer_session" (
+    "id" bigint generated always as identity primary key,
+    "role" varchar(150) not null default 'USER',
+    "customer_id" bigint not null,
+    "name" varchar(255) not null,
+    "refresh_token" text not null,
+    "expiration" timestamptz not null,
+    "revoked" boolean not null default false,
+    "created_at" timestamptz default now(),
+    "updated_at" timestamptz default now(),
+    "deleted_at" timestamptz default null,
+    "created_by" varchar(255) not null,
+    "updated_by" varchar(255) not null,
+    "deleted_by" varchar(255) default null
+);
+
 drop table restaurant_document, restaurant_document_aud;
 
 alter table restaurant add column document_type varchar(4) not null;
